cb1da16c09994a3c3a919731c0e2e353ae38945a
ankit-s0360 <ankitsoni0360@gmail.com>
2024-09-05 00:21:30 +0530

    changed initialstate

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -9,7 +9,7 @@ import toast from "react-hot-toast";
 const initialState = {
     isLoggedIn: localStorage.getItem('isLoggedIn') || false,
     role: localStorage.getItem('role') || "",
-    data: localStorage.getItem('data') != undefined ? JSON.parse(localStorage.getItem('data')) : {},
+    data: localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : {},
 };
 
 export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
